ac8db099b0edea55c703dfaaad52a955afdb7a9a
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-17 01:27:53 +0800

    finish line exit effect

--- htextview-library/src/main/java/com/hanks/htextview/util/LineText.java
+++ htextview-library/src/main/java/com/hanks/htextview/util/LineText.java
@@ -33,25 +33,35 @@ public class LineText implements AnimateText {
 
     int textColor = Color.WHITE;
     Paint linePaint;
-
+    float padding = 20;
+    float gap     = 0;
+    PointF p1 = new PointF();
+    PointF p2 = new PointF();
+    PointF p3 = new PointF();
+    PointF p4 = new PointF();
+    PointF p5 = new PointF();
+    PointF p6 = new PointF();
+    PointF p7 = new PointF();
+    PointF p8 = new PointF();
+    int xLineLength = 0;
     private DisplayMetrics metrics;
     private CharSequence   mText;
     private CharSequence   mOldText;
-
     private List<CharacterDiffResult> differentList = new ArrayList<>();
-
     private float textSize;
-
     private float startX    = 0;
     private float startY    = 0;
     private float oldStartX = 0;
-
     private float[] gaps    = new float[100];
     private float[] oldGaps = new float[100];
     private float distWidth;
     private float distHeight;
+
     private float yLineLength;
     private float lineWidth = 2;
+
+    private int yLineShort;
+    private int xLineShort;
 
     public void init(HTextView hTextView) {
         mHTextView = hTextView;
@@ -105,43 +115,54 @@ public class LineText implements AnimateText {
         mHTextView.invalidate();
     }
 
-    float  padding = 20;
-    PointF p1      = new PointF();
-    PointF  p2      = new PointF();
-    PointF  p3      = new PointF();
-    PointF  p4      = new PointF();
-
-    int xLineLength = 0;
-
     @Override public void onDraw(Canvas canvas) {
         float offset = startX;
         float percent = progress;
         paint.setAlpha(255);
         paint.setTextSize(textSize);
 
-        xLineLength = (int) (mHTextView.getWidth() - (mHTextView.getWidth()- distWidth) * percent);
-        yLineLength = (int) (mHTextView.getHeight() - (mHTextView.getHeight()- distHeight) * percent);
+        xLineLength = (int) (mHTextView.getWidth() - (mHTextView.getWidth() - distWidth + gap) * percent);
+        yLineLength = (int) (mHTextView.getHeight() - (mHTextView.getHeight() - distHeight + gap) * percent);
 
-        p1.x = (mHTextView.getWidth() /2 + distWidth /2 )* percent;
+        p1.x = (mHTextView.getWidth() / 2 + distWidth / 2 - gap) * percent;
         p1.y = (mHTextView.getHeight() - distHeight) / 2;
         canvas.drawLine(p1.x - xLineLength, p1.y, p1.x, p1.y, paint);
 
-        p2.x = (mHTextView.getWidth() /2 + distWidth /2 );
-        p2.y = (mHTextView.getHeight()/2 + distHeight / 2) * percent;
+        p2.x = (mHTextView.getWidth() / 2 + distWidth / 2);
+        p2.y = (mHTextView.getHeight() / 2 + distHeight / 2 - gap) * percent;
         canvas.drawLine(p2.x, p2.y - yLineLength, p2.x, p2.y, paint);
 
-        p3.x = mHTextView.getWidth() -  (mHTextView.getWidth()/2+distWidth/2 ) * percent;
-        p3.y = (mHTextView.getHeight() + distHeight) / 2 ;
+        p3.x = mHTextView.getWidth() - (mHTextView.getWidth() / 2 + distWidth / 2 - gap) * percent;
+        p3.y = (mHTextView.getHeight() + distHeight) / 2;
         canvas.drawLine(p3.x + xLineLength, p3.y, p3.x, p3.y, paint);
 
-        p4.x = (mHTextView.getWidth() /2 - distWidth /2 );
-        p4.y = mHTextView.getHeight() - (mHTextView.getHeight()/2 + distHeight/2 ) * percent;
+        p4.x = (mHTextView.getWidth() / 2 - distWidth / 2);
+        p4.y = mHTextView.getHeight() - (mHTextView.getHeight() / 2 + distHeight / 2 - gap) * percent;
         canvas.drawLine(p4.x, p4.y + yLineLength, p4.x, p4.y, paint);
 
 
+
+        xLineShort = (int) ( (distWidth + gap) * (1-percent));
+        yLineShort = (int) ( (distHeight + gap) * (1-percent));
+
+
+        p5.x = (mHTextView.getWidth() / 2 + distWidth / 2 ) ;
+        p5.y = (mHTextView.getHeight() - distHeight) / 2;
+        canvas.drawLine(p5.x - xLineShort, p5.y, p5.x, p5.y, paint);
+
+        p6.x = (mHTextView.getWidth() / 2 + distWidth / 2);
+        p6.y = (mHTextView.getHeight() / 2 + distHeight / 2) ;
+        canvas.drawLine(p6.x, p6.y - yLineShort, p6.x, p6.y, paint);
+
+        p7.x = mHTextView.getWidth() - (mHTextView.getWidth() / 2 + distWidth / 2 - gap);
+        p7.y = (mHTextView.getHeight() + distHeight) / 2;
+        canvas.drawLine(p7.x + xLineShort, p7.y, p7.x, p7.y, paint);
+
+        p8.x = (mHTextView.getWidth() / 2 - distWidth / 2);
+        p8.y = mHTextView.getHeight() - (mHTextView.getHeight() / 2 + distHeight / 2 - gap) ;
+        canvas.drawLine(p8.x, p8.y + yLineShort, p8.x, p8.y, paint);
+
         canvas.drawText(mText, 0, mText.length(), offset, startY, paint);
-
-
 
         for (int i = 0; i < mText.length(); i++) {
             offset += gaps[i];
