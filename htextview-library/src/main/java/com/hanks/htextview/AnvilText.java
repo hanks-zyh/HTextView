c195adbb7878306445f8745094d9a274b2771cd4
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-17 22:14:56 +0800

    add Anvil scale bitmap

--- htextview-library/src/main/java/com/hanks/htextview/AnvilText.java
+++ htextview-library/src/main/java/com/hanks/htextview/AnvilText.java
@@ -1,16 +1,11 @@
 package com.hanks.htextview;
 import android.animation.ValueAnimator;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.WindowManager;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.BounceInterpolator;
 
 import com.hanks.htextview.util.CharacterUtils;
@@ -19,38 +14,42 @@ import com.hanks.htextview.util.HLog;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 /**
- * 蒸发效果
+ * keynote 轰然坠落效果
  * Created by hanks on 15-12-14.
  */
 public class AnvilText implements AnimateText {
 
-    Paint paint, oldPaint,bitmapPaint;
-    float charTime  = 500; // 每个字符动画时间 500ms
-    int   mostCount = 20; // 最多10个字符同时动画
+    Paint paint, oldPaint, bitmapPaint;
+
+    float ANIMA_DURATION = 800; // 每个字符动画时间 500ms
+
     HTextView mHTextView;
-    float upDistance = 0;
+    float    upDistance = 0;
+    Bitmap[] smokes     = new Bitmap[50];
+
+    float progress = 0;
+
     private float[] gaps    = new float[100];
     private float[] oldGaps = new float[100];
-    private DisplayMetrics metrics;
-    private float          textSize;
-    private CharSequence   mText;
-    private CharSequence   mOldText;
+
+    private float textSize;
+
+    private CharSequence mText;
+    private CharSequence mOldText;
+
     private List<CharacterDiffResult> differentList = new ArrayList<>();
-    private float                     oldStartX     = 0;
-    private float                     startX        = 0;
-    private float                     startY        = 0;
-    private Bitmap smokeBitmap;
 
-
-    Bitmap[] smokes = new Bitmap[50];
+    private float oldStartX = 0;
+    private float startX    = 0;
+    private float startY    = 0;
+    private int textWidth;
 
     public void init(HTextView hTextView) {
         mHTextView = hTextView;
 
-        mText = "";
-        mOldText = "";
+        mText = mHTextView.getText();
+        mOldText = mHTextView.getText();
 
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.WHITE);
@@ -64,27 +63,19 @@ public class AnvilText implements AnimateText {
         bitmapPaint.setColor(Color.WHITE);
         bitmapPaint.setStyle(Paint.Style.FILL);
 
-        metrics = new DisplayMetrics();
-        WindowManager windowManger = (WindowManager) hTextView.getContext()
-                .getSystemService(Context.WINDOW_SERVICE);
-        windowManger.getDefaultDisplay().getMetrics(metrics);
-
         textSize = hTextView.getTextSize();
 
-        smokeBitmap = BitmapFactory.decodeResource(mHTextView.getResources(), R.drawable.smoke);
-
-
         try {
-            String pp = "";
             R.drawable d = new R.drawable();
             for (int j = 0; j < 50; j++) {
+                String drawable = "";
                 if (j < 10) {
-                    pp = "0";
+                    drawable = "wenzi000" + j;
                 } else {
-                    pp = "";
+                    drawable = "wenzi00" + j;
                 }
-                Field fieldimgId = d.getClass().getDeclaredField("wenzi00"+ pp + j);
-                int imgId = (Integer) fieldimgId.get(d);//这个ID就是每个图片资源ID
+                Field fieldImgId = d.getClass().getDeclaredField(drawable);
+                int imgId = (Integer) fieldImgId.get(d);//这个ID就是每个图片资源ID
                 smokes[j] = BitmapFactory.decodeResource(hTextView.getResources(), imgId);
             }
         } catch (Exception e) {
@@ -104,24 +95,25 @@ public class AnvilText implements AnimateText {
 
         calc();
 
-        int n = mText.length();
-        n = n <= 0 ? 1 : n;
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1)
+                .setDuration((long) ANIMA_DURATION);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override public void onAnimationUpdate(ValueAnimator animation) {
+                progress = (float) animation.getAnimatedValue();
+                mHTextView.invalidate();
+            }
+        });
+        valueAnimator.start();
 
-        // 计算动画总时间
-//        long duration = (long) (charTime + charTime / mostCount * (n - 1));
-//
-//        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, duration).setDuration(duration);
-//        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override public void onAnimationUpdate(ValueAnimator animation) {
-//                progress = (float) animation.getAnimatedValue();
-//                mHTextView.invalidate();
-//            }
-//        });
-//        valueAnimator.start();
+        for (int i = 0; i < smokes.length; i++) {
+            Bitmap smoke = smokes[i];
 
-        progress = 0;
-        mHTextView.invalidate();
+            int dstWidth = (int) (textWidth * 1.5f);
+            if(dstWidth<400) dstWidth = 400;
+            int dstHeight = (int) (smoke.getHeight() * 1f / smoke.getWidth() * dstWidth);
+            smokes[i] = Bitmap.createScaledBitmap(smoke, dstWidth, dstHeight, false);
+            smoke.recycle();
+        }
     }
 
     @Override public void onDraw(Canvas canvas) {
@@ -130,13 +122,11 @@ public class AnvilText implements AnimateText {
 
         int maxLength = Math.max(mText.length(), mOldText.length());
 
-//        float percent = progress / (charTime + charTime / mostCount * (mText.length() - 1)); // 动画进行的百分比 0~1
-        float percent = progress*1.0f / 50f; // 动画进行的百分比 0~1
+        float percent = progress; // 动画进行的百分比 0~1
 
-        HLog.i("动画进度:"+percent);
-        float firstNew = -1;
-        float lastNew = -1;
+        HLog.i("动画进度:" + percent);
 
+        boolean showSmoke = false;
         for (int i = 0; i < maxLength; i++) {
 
             // draw old text
@@ -152,14 +142,6 @@ public class AnvilText implements AnimateText {
                     canvas.drawText(mOldText.charAt(i) + "", 0, 1, distX, startY, oldPaint);
                 } else {
 
-                    // 记录第一个新产生的字符和最后一个产生的字符位置,计算出现烟的中心
-                    if(firstNew == -1){
-                        firstNew = offset;
-                    }
-
-                    lastNew = offset;
-
-
                     float p = percent * 2f;
                     p = p > 1 ? 1 : p;
                     oldPaint.setAlpha((int) ((1 - p) * 255));
@@ -173,11 +155,8 @@ public class AnvilText implements AnimateText {
 
                 if (!CharacterUtils.stayHere(i, differentList)) {
 
+                    showSmoke = true;
                     float interpolation = new BounceInterpolator().getInterpolation(percent);
-
-                    int alpha = (int) (255 * percent);
-                    alpha = alpha > 255 ? 255 : alpha;
-                    alpha = alpha < 0 ? 0 : alpha;
 
                     paint.setAlpha(255);
                     paint.setTextSize(textSize);
@@ -192,61 +171,36 @@ public class AnvilText implements AnimateText {
             }
         }
 
-        if (percent>0.3 && percent < 1 ) {
-//            if(firstNew != -1 ) {
-                drawSmokes(canvas,startX + (offset - startX)/2f, startY-50, paint,percent);
-//            }
-        }
-
-        if(progress<50) {
-            mHTextView.postInvalidate();
-            progress++;
+        if (percent > 0.3 && percent < 1) {
+            if (showSmoke) {
+                drawSmokes(canvas, startX + (offset - startX) / 2f, startY - 50, paint, percent);
+            }
         }
     }
 
-    int progress = 0;
     /**
      * 烟雾, 从中间向外扩散
+     *
      * @param canvas 画布
-     * @param x 中心点x坐标
-     * @param y 中心点Y坐标
-     * @param paint 画笔
+     * @param x      中心点x坐标
+     * @param y      中心点Y坐标
+     * @param paint  画笔
      */
-    private void drawSmokes(Canvas canvas, float x, float y, Paint paint,float percent) {
-//        float[] positions = new float[]{-10,-8,-6,-5,-4,-3,-2.5f,-2,-1.5f,-1,-1,0,0,0,1,1,1.5f,2,2.5f,3,4,5,6,8,10};
-//        Random random = new Random();
-//        for (int i=0;i<positions.length;i++) {
-//            Bitmap singleSmoke = getSingleSmoke(positions[i],random);
-//            paint.setAlpha((int) (255*( (Math.abs(positions[i])-10)/10f)));
-//            canvas.drawBitmap(singleSmoke, x + positions[i] * 20, y + (50 -singleSmoke.getHeight()), paint);
-//        }
-
-        if (progress < 50) {
-            Bitmap b = smokes[0];
-
-            try {
-                int index = (int) (50 * percent);
-                if (index<0) index = 0;
-                if(index>=50) index = 49;
-                b = smokes[index];
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            if (b != null) {
-                int w = b.getWidth();
-                int h = b.getHeight();
-
-                canvas.drawBitmap(b, x- w/2, y-h/2 , bitmapPaint);
-            }
+    private void drawSmokes(Canvas canvas, float x, float y, Paint paint, float percent) {
+        Bitmap b = smokes[0];
+        try {
+            int index = (int) (50 * percent);
+            if (index < 0) index = 0;
+            if (index >= 50) index = 49;
+            b = smokes[index];
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    private Bitmap getSingleSmoke(float i, Random random) {
-
-        int size = (int) (Math.abs(i)* random.nextDouble() * 15 +30);
-
-        return Bitmap.createScaledBitmap(smokeBitmap, size,size,false);
+        if (b != null) {
+            int w = b.getWidth();
+            int h = b.getHeight();
+            canvas.drawBitmap(b, x - w / 2, y - h / 2, bitmapPaint);
+        }
     }
 
     private void calc() {
@@ -274,8 +228,7 @@ public class AnvilText implements AnimateText {
         Rect bounds = new Rect();
         paint.getTextBounds(mText.toString(), 0, mText.length(), bounds);
         upDistance = bounds.height();
+        textWidth = bounds.width();
     }
-
-
 
 }
