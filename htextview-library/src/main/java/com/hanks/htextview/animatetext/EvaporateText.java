142828f2b77e6b8a5f1498b19d7c57d661db5cbd
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-19 14:08:20 +0800

    change EvaporateText to extend HText remove unused code

--- htextview-library/src/main/java/com/hanks/htextview/animatetext/EvaporateText.java
+++ htextview-library/src/main/java/com/hanks/htextview/animatetext/EvaporateText.java
@@ -1,78 +1,25 @@
 package com.hanks.htextview.animatetext;
 import android.animation.ValueAnimator;
-import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
-import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
-import com.hanks.htextview.HTextView;
 import com.hanks.htextview.util.CharacterUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 /**
  * 蒸发效果
  * Created by hanks on 15-12-14.
  */
-public class EvaporateText implements AnimateText {
+public class EvaporateText extends HText {
 
-    float progress = 0;
-    Paint paint, oldPaint;
     float charTime  = 300; // 每个字符动画时间 500ms
     int   mostCount = 20; // 最多10个字符同时动画
-    HTextView mHTextView;
-    float upDistance = 0;
-    private float[] gaps    = new float[100];
-    private float[] oldGaps = new float[100];
-    private DisplayMetrics metrics;
-    private float          textSize;
-    private CharSequence   mText;
-    private CharSequence   mOldText;
-    private List<CharacterDiffResult> differentList = new ArrayList<>();
-    private float oldStartX = 0;
-    private float startX    = 0;
-    private float startY    = 0;
+    private int upDistance;
 
-    public void init(HTextView hTextView, AttributeSet attrs, int defStyle) {
-        mHTextView = hTextView;
-
-        mText = "";
-        mOldText = "";
-
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Paint.Style.FILL);
-
-        oldPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        oldPaint.setColor(Color.BLACK);
-        oldPaint.setStyle(Paint.Style.FILL);
-
-        metrics = new DisplayMetrics();
-        WindowManager windowManger = (WindowManager) hTextView.getContext()
-                .getSystemService(Context.WINDOW_SERVICE);
-        windowManger.getDefaultDisplay().getMetrics(metrics);
-
-        textSize = hTextView.getTextSize();
+    @Override protected void initVariables() {
 
     }
 
-    @Override public void reset(CharSequence text) {
-        progress = 1;
-        calc();
-        mHTextView.invalidate();
-    }
-
-    @Override public void animateText(CharSequence text) {
-        mOldText = mText;
-        mText = text;
-
-        calc();
-
+    @Override protected void animateStart(CharSequence text) {
         int n = mText.length();
         n = n <= 0 ? 1 : n;
 
@@ -90,6 +37,17 @@ public class EvaporateText implements AnimateText {
         valueAnimator.start();
     }
 
+    @Override protected void animatePrepare(CharSequence text) {
+
+        Rect bounds = new Rect();
+        mPaint.getTextBounds(mText.toString(), 0, mText.length(), bounds);
+        upDistance = bounds.height();
+    }
+
+    @Override protected void drawFrame(Canvas canvas) {
+
+    }
+
     @Override public void onDraw(Canvas canvas) {
         float offset = startX;
         float oldOffset = oldStartX;
@@ -103,19 +61,19 @@ public class EvaporateText implements AnimateText {
                 //
                 float pp = progress / (charTime + charTime / mostCount * (mText.length() - 1));
 
-                oldPaint.setTextSize(textSize);
+                mOldPaint.setTextSize(mTextSize);
                 int move = CharacterUtils.needMove(i, differentList);
                 if (move != -1) {
-                    oldPaint.setAlpha(255);
+                    mOldPaint.setAlpha(255);
                     float p = pp * 2f;
                     p = p > 1 ? 1 : p;
                     float distX = CharacterUtils.getOffset(i, move, p, startX, oldStartX, gaps, oldGaps);
-                    canvas.drawText(mOldText.charAt(i) + "", 0, 1, distX, startY, oldPaint);
+                    canvas.drawText(mOldText.charAt(i) + "", 0, 1, distX, startY, mOldPaint);
                 } else {
-                    oldPaint.setAlpha((int) ((1 - pp) * 255));
+                    mOldPaint.setAlpha((int) ((1 - pp) * 255));
                     float y = startY - pp * upDistance;
-                    float width = oldPaint.measureText(mOldText.charAt(i) + "");
-                    canvas.drawText(mOldText.charAt(i) + "", 0, 1, oldOffset + (oldGaps[i] - width) / 2, y, oldPaint);
+                    float width = mOldPaint.measureText(mOldText.charAt(i) + "");
+                    canvas.drawText(mOldText.charAt(i) + "", 0, 1, oldOffset + (oldGaps[i] - width) / 2, y, mOldPaint);
                 }
                 oldOffset += oldGaps[i];
             }
@@ -129,18 +87,14 @@ public class EvaporateText implements AnimateText {
                     alpha = alpha > 255 ? 255 : alpha;
                     alpha = alpha < 0 ? 0 : alpha;
 
-                    float size = textSize * 1f / charTime * (progress - charTime * i / mostCount);
-                    //                  float size = (textSize * progress * 6 - i * 5 * textSize/(mText.length()-1));
-                    size = size > textSize ? textSize : size;
-                    size = size < 0 ? 0 : size;
 
-                    paint.setAlpha(alpha);
-                    paint.setTextSize(textSize);
+                    mPaint.setAlpha(alpha);
+                    mPaint.setTextSize(mTextSize);
                     float pp = progress / (charTime + charTime / mostCount * (mText.length() - 1));
                     float y = upDistance + startY - pp * upDistance;
 
-                    float width = paint.measureText(mText.charAt(i) + "");
-                    canvas.drawText(mText.charAt(i) + "", 0, 1, offset + (gaps[i] - width) / 2, y, paint);
+                    float width = mPaint.measureText(mText.charAt(i) + "");
+                    canvas.drawText(mText.charAt(i) + "", 0, 1, offset + (gaps[i] - width) / 2, y, mPaint);
                 }
 
                 offset += gaps[i];
@@ -148,30 +102,4 @@ public class EvaporateText implements AnimateText {
         }
     }
 
-    private void calc() {
-        textSize = mHTextView.getTextSize();
-        paint.setTextSize(textSize);
-
-        for (int i = 0; i < mText.length(); i++) {
-            gaps[i] = paint.measureText(mText.charAt(i) + "");
-        }
-
-        oldPaint.setTextSize(textSize);
-
-        for (int i = 0; i < mOldText.length(); i++) {
-            oldGaps[i] = oldPaint.measureText(mOldText.charAt(i) + "");
-        }
-
-        oldStartX = (mHTextView.getWidth() - oldPaint.measureText(mOldText.toString())) / 2f;
-
-        startX = (mHTextView.getWidth() - paint.measureText(mText.toString())) / 2f;
-        startY = (int) ((mHTextView.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2));
-
-        differentList.clear();
-        differentList.addAll(CharacterUtils.diff(mOldText, mText));
-
-        Rect bounds = new Rect();
-        paint.getTextBounds(mText.toString(), 0, mText.length(), bounds);
-        upDistance = bounds.height();
-    }
 }
