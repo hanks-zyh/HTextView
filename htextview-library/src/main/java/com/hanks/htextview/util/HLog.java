01c14ec8ad7ee3dc28552afe4ced79032e0d01b5
hanks-zyh <zhangyuhan2014@gmail.com>
2015-12-26 23:54:13 +0800

    manage log

--- htextview-library/src/main/java/com/hanks/htextview/util/HLog.java
+++ htextview-library/src/main/java/com/hanks/htextview/util/HLog.java
@@ -1,10 +1,15 @@
 package com.hanks.htextview.util;
 import android.util.Log;
+
+import com.hanks.htextview.BuildConfig;
+
 /**
  * Created by hanks on 15-12-14.
  */
 public class HLog {
     public static void i(Object s){
-        Log.i("HLog",s.toString());
+        if(BuildConfig.DEBUG) {
+            Log.i("HLog", s.toString());
+        }
     }
 }
